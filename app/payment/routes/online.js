72692d6e42d1e77004c8d3b2b037867e6df9c8cb
wassimkha <wassim.khadim@mail.mcgill.ca>
2021-03-22 22:22:39 -0400

    microservices com with ingress

--- app/payment/routes/online.js
+++ app/payment/routes/online.js
@@ -7,13 +7,17 @@ get_counter = 0;
 router.get('/online', async (req, res) => {
     console.log(`Get request received # ${get_counter} to the payment service`);
     get_counter += 1;
-    res.json({online: true, message: 'payment service online 1'});
+    res.json({online: true, message: 'payment service online'});
 })
 
 router.get('/user-online', async (req, res) => {
-    const data = await axios.post('http://auth-srv:3000/auth/is-user-online', {
+    const data = await axios.post('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/auth/is-user-online', {
         user_id: 'wassim'
-    })
+    }, {
+        headers: {
+            'Host': `backend.com`
+        }}
+    )
     res.json({online: true, response_auth: data.data});
 })
 
